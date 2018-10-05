5247edd70ff2dffcbba19fc6cf3e7739c6299c35
akudai <akudai@go.olemiss.edu>
2018-10-04 19:38:35 -0500

    Adding the source code for CRUD

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Insert_Update_Delete
+{
+    partial class InsertUpdateDelete
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.update = new System.Windows.Forms.Button();
+            this.txtInsert = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(595, 220);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 3;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(113, 68);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 4;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(57, 113);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 186);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(183, 113);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(215, 186);
+            this.listBox2.TabIndex = 6;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(634, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(555, 220);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(34, 20);
+            this.txtId.TabIndex = 11;
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(544, 254);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 23);
+            this.update.TabIndex = 12;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // txtInsert
+            // 
+            this.txtInsert.Location = new System.Drawing.Point(42, 42);
+            this.txtInsert.Name = "txtInsert";
+            this.txtInsert.Size = new System.Drawing.Size(100, 20);
+            this.txtInsert.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(564, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(619, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(235, 9);
+            this.label6.MaximumSize = new System.Drawing.Size(500, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Auditors";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(73, 39);
+            this.label7.MaximumSize = new System.Drawing.Size(500, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Add New Auditors";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInsert);
+            this.groupBox1.Controls.Add(this.Insert);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(509, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // InsertUpdateDelete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 391);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtname);
+            this.Name = "InsertUpdateDelete";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.InsertUpdateDelete_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.TextBox txtInsert;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Insert_Update_Delete
+{ 
+    public partial class InsertUpdateDelete : Form
+    {
+
+        SqlConnection cn = new SqlConnection(@"Data Source=LAPTOP-3AT0919R\SQLEXPRESS;Integrated Security=True");
+
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        public InsertUpdateDelete()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertUpdateDelete_Load(object sender, EventArgs e)
+        {
+            cmd.Connection = cn;
+            loadlist();
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            if(txtInsert.Text != "")
+            {
+                cn.Open();
+                cmd.CommandText = "insert into Level1 (EmpName) values ('" + txtInsert.Text + "')";
+                cmd.ExecuteNonQuery();
+                cmd.Clone();
+                MessageBox.Show("Record Inserted!", "Aamir Kudai's Application");
+                cn.Close();
+                txtInsert.Text = "";
+                loadlist();
+            }
+        }
+
+        private void loadlist()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            cn.Open();
+            cmd.CommandText = "select * from Level1";
+            dr = cmd.ExecuteReader();
+            if(dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    listBox1.Items.Add(dr[0].ToString());
+                    listBox2.Items.Add(dr[1].ToString());
+                }
+            }
+            cn.Close();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ls = sender as ListBox;
+            if(ls.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = ls.SelectedIndex;
+                listBox2.SelectedIndex = ls.SelectedIndex;
+                txtId.Text = listBox1.SelectedItem.ToString();
+                txtname.Text = listBox2.SelectedItem.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text != "")
+            {
+                cn.Open();
+                cmd.CommandText = "delete from Level1 where id='" + txtId.Text + "'";
+                cmd.ExecuteNonQuery();
+                cmd.Clone();
+                MessageBox.Show("Record Deleted!", "Aamir Kudai's Application");
+                cn.Close();
+                txtId.Text = "";
+                txtname.Text = "";
+                loadlist();
+            }
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text != "")
+            {
+                cn.Open();
+                cmd.CommandText = "update Level1 set EmpName='" + txtname.Text + "' where id='" + txtId.Text+"'";
+                cmd.ExecuteNonQuery();
+                cmd.Clone();
+                MessageBox.Show("Record Updated!", "Aamir Kudai's Application");
+                cn.Close();
+                txtId.Text = "";
+                txtname.Text = "";
+                loadlist();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
