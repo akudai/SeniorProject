98f9d5c00f6134df4722393b44443f24cb909b90
akudai <akudai@go.olemiss.edu>
2018-10-24 01:59:37 -0500

    added a CRUD Operation windows web form application

---
+++ SchedulingApplication_10-23-2018/SchedulingApplication_10-23-2018/auditors.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="auditors.aspx.cs" Inherits="SchedulingApplication_10_23_2018.auditors" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Auditors</h1>
+            <h2>Welcome to the auditors page</h2>
+            <asp:HiddenField ID="hfAuditorID" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Auditor Name"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Auditor Level"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtLevel" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="Auditor Level"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtleveldemo" runat="server"></asp:TextBox>
+                        <asp:RadioButtonList ID="rdLevel" runat="server" RepeatLayout="Flow">
+                            <asp:ListItem Value="1">Level 1</asp:ListItem>
+                            <asp:ListItem Value="2">Level 2</asp:ListItem>
+                            <asp:ListItem Value="2">Level 3</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td colspan="2">
+                         <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                         <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                         <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblSuccessMessage" runat="server" Text="" ForeColor="Green"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblErrorMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="gvAuditor" runat="server" AutoGenerateColumns="false" AllowSorting="True">
+                <Columns>
+                    <asp:BoundField DataField="AuditorID" HeaderText="ID" />
+                    <asp:BoundField DataField="AuditorName" HeaderText="Name" />
+                    <asp:BoundField DataField="AuditorLevel" HeaderText="Level" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkView" runat="server" CommandArgument='<%# Eval("AuditorID") %>' OnClick="lnk_OnClick" >Modify</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ SchedulingApplication_10-23-2018/SchedulingApplication_10-23-2018/auditors.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SchedulingApplication_10_23_2018
+{
+    public partial class auditors : System.Web.UI.Page
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-3AT0919R\SQLEXPRESS;Initial Catalog=seniorProject;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) //avoids executing first Time
+            {
+            btnDelete.Enabled = false;
+            FillGridView();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            hfAuditorID.Value = "";
+            txtName.Text = txtLevel.Text = "";
+            lblErrorMessage.Text = lblSuccessMessage.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if(sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlCommand sqlCmd = new SqlCommand("AuditorCreateOrUpdate", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@AuditorID",(hfAuditorID.Value==""?0:Convert.ToInt32(hfAuditorID.Value)));
+            sqlCmd.Parameters.AddWithValue("@AuditorName", txtName.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@AuditorLevel", txtLevel.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            string contactID = hfAuditorID.Value;
+            Clear();
+            if (contactID == "")
+                lblSuccessMessage.Text = "Saved Successfully";
+            else
+                lblSuccessMessage.Text = "Updated Successfully";
+
+            FillGridView();
+        }
+
+        void FillGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlDataAdapter sqlDa = new SqlDataAdapter("AuditorViewAll", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlCon.Close();
+            gvAuditor.DataSource = dtbl;
+            gvAuditor.DataBind();
+        }
+
+        protected void lnk_OnClick(object sender, EventArgs e)
+        {
+            int AuditorID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlDataAdapter sqlDa = new SqlDataAdapter("AuditorViewByID", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@AuditorID", AuditorID);
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlCon.Close();
+            hfAuditorID.Value = AuditorID.ToString();
+            txtName.Text = dtbl.Rows[0]["AuditorName"].ToString();
+            txtLevel.Text = dtbl.Rows[0]["AuditorLevel"].ToString();
+            btnSave.Text = "Update " + txtName.Text;
+            btnDelete.Enabled = true;
+            //newly added
+            txtleveldemo.Text = rdLevel.SelectedItem.Value.ToString();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlCommand sqlCmd = new SqlCommand("AuditorDeleteByID", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@AuditorID", Convert.ToInt32(hfAuditorID.Value));
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            Clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Deleted Sucessfully";
+        }
+    }
+}
